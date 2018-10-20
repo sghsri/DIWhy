4f8f367c16100c9bd1a65979554e3578c6d2e77f
10001shh <sghsri@gmail.com>
2018-10-20 14:15:50 -0500

    categories request

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
-const request = require('request');
 const express = require('express');
 const app = express();
+var unirest = require('unirest');
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
+
+unirest.get("https://devru-instructables.p.mashape.com/json-api/getCategories")
+    .header("X-Mashape-Key", "NchdEVDYyvmshQWZFToOl688Ib07p1tO5pPjsn0Rzzw9xT9IxG")
+    .header("Accept", "application/json")
+    .end(function (result) {
+        var categories = result.body;
+        console.log(categories.workshop);
+    });
 app.get('/', (req, res) => {
     res.send("hello world");
 });
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Listening on port: ${port}`);
-});
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//     console.log(`Listening on port: ${port}`);
+// });
