5af0c5bd387ff2db61eb5f6f624bee2fc2ddc09a
chatcher21 <carolinewhatcher@gmail.com>
2018-10-20 14:25:27 -0500

    Update webscraping.js

--- webscraping.js
+++ webscraping.js
@@ -1,1 +1,12 @@
+const rp = require('request-promise');
+const $ = require('cheerio');
+var product_name = "wood glue"
+var url = 'https://www.homedepot.com/s/' + product_name + '?NCNI-5';
 
+rp(url)
+  .then(function(html) {
+    console.log(html);
+  })
+  .catch(function(err) {
+    //handle error
+});
